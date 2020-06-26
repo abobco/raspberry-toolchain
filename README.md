e4744285dbf9326cf7eaa7c226f5d9d691b6654a
Austin Bobco <abobco@gmail.com>
2020-06-26 12:11:01 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# raspberry pi CMake toolchain
+
+This is a CMake toolchain file & example project for cross compiling C/C++ code to arm hard float processors such as those in raspberry pis.
+
+## Setup
+
+Windows
+-----------------------
+1. Download and install [clang](https://releases.llvm.org/download.html) to your system path. You can either download a prebuilt binary from that link, or download it from the LLVM git & build it from source
+
+2. Download & install a [raspberry pi toolchain](https://gnutoolchains.com/raspberry/). At the time of writing, the **raspberry-gcc8.3.0-r2.exe** link is probably what you want
+
+3. Install the [ninja build system](https://github.com/ninja-build/ninja/releases) to your system path
+
+4. Install [CMake](https://cmake.org/download/) to your system path
+
+5. Clone this repo, open powershell in the root of your clone and run this: 
+
+``````````````````````````````````````````````````
+mkdir build
+cd build
+cmake -GNinja -D TOOLCHAIN_PATH="path/to/your/SysGCC" ..
+cmake --build .
+```````````````````````````````````````````````````
+NOTE: you must change **"path/to/your/SysGCC"** in the 3rd line to point to wherever you installed your raspberry pi toolchain. 
+
+If everything worked correctly, you should now have a binary called `example` in the `build` folder that you can copy & run on your raspberry pi. Hooray!
---
+++ armhf-toolchain.cmake
@@ -0,0 +1,42 @@
+# arm-hardfloat toolchain file using clang with a prebuilt raspberry-pi sysroot/linker
+
+# ------------------------ non portable part ------------------------------------
+
+# give path to prebuilt toolchain
+set(TOOLCHAIN_PATH "D:/SysGCC" CACHE PATH "Path to your raspberry toolchain")
+set(GCC_ARM_TOOLCHAIN "${TOOLCHAIN_PATH}/raspberry/arm-linux-gnueabihf")
+set(GCC_ARM_LINKER_EXE "${TOOLCHAIN_PATH}/raspberry/bin/arm-linux-gnueabihf-ld.exe")
+SET(TARGET_TRIPLE arm-linux-gnueabihf)
+
+# -------------------------------------------------------------------------------
+
+set(CMAKE_CROSSCOMPILING TRUE)
+SET(CMAKE_SYSTEM_NAME Linux)
+SET(CMAKE_SYSTEM_PROCESSOR arm)
+
+# get the toolchain sysroot
+LIST(APPEND CMAKE_PROGRAM_PATH ${GCC_ARM_TOOLCHAIN})
+SET(CMAKE_SYSROOT ${GCC_ARM_TOOLCHAIN}/sysroot)
+
+# set compilers
+SET(CMAKE_C_COMPILER clang)
+SET(CMAKE_C_COMPILER_TARGET ${TARGET_TRIPLE})
+SET(CMAKE_CXX_COMPILER clang++)
+SET(CMAKE_CXX_COMPILER_TARGET ${TARGET_TRIPLE})
+set(CMAKE_ASM_COMPILER clang)
+set(CMAKE_ASM_COMPILER_TARGET ${TARGET_TRIPLE})
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+# linker flags
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fuse-ld=${GCC_ARM_LINKER_EXE}")
+set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fuse-ld=${GCC_ARM_LINKER_EXE}")
+
+# cross compiler flags
+set(CMAKE_C_FLAGS " ${CMAKE_C_FLAGS} --target=${TARGET_TRIPLE} --sysroot=${CMAKE_SYSROOT}")
+
+# Search for programs in the build host directories
+set(CMAKE_FIND_ROOT_PATH ${CMAKE_SYSROOT})
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# For libraries and headers in the target directories
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
